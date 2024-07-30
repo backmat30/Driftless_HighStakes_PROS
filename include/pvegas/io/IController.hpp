e5c60b1c5f366d57cddf5d2deae6fff57103df22
Matthew Backman <backmanm@uwplatt.edu>
2024-07-30 01:47:37 -0500

    formatted IController

--- include/pvegas/io/IController.hpp
+++ include/pvegas/io/IController.hpp
@@ -4,23 +4,24 @@
 #include "EControllerAnalog.hpp"
 #include "EControllerDigital.hpp"
 #include <string>
-namespace pvegas{
-    namespace io{
-        class IController{
-            virtual ~IController() = 0;
+namespace pvegas {
+namespace io {
+class IController {
+public:
+  virtual ~IController() = 0;
 
-            virtual void init() = 0;
+  virtual void init() = 0;
 
-            virtual void run() = 0;
+  virtual void run() = 0;
 
-            virtual double getAnalog(EControllerAnalog channel) = 0;
+  virtual double getAnalog(EControllerAnalog channel) = 0;
 
-            virtual bool getDigital(EControllerDigital channel) = 0;
+  virtual bool getDigital(EControllerDigital channel) = 0;
 
-            virtual bool getNewDigital(EControllerDigital channel) = 0;
+  virtual bool getNewDigital(EControllerDigital channel) = 0;
 
-            virtual void rumble(std::string pattern) = 0;
-        };
-    }
-}
+  virtual void rumble(std::string pattern) = 0;
+};
+} // namespace io
+} // namespace pvegas
 #endif
