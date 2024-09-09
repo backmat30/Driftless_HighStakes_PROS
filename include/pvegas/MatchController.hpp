2d8f4871e0f1b50a8adeec4ef1911001ce57599b
Matthew Backman <backmanm@uwplatt.edu>
2024-09-09 16:24:07 -0500

    Adds outline for match controller class

--- include/pvegas/MatchController.hpp
+++ include/pvegas/MatchController.hpp
@@ -1,13 +1,44 @@
 #ifndef __MATCHCONTROLLER_HPP__
 #define __MATCHCONTROLLER_HPP__
 
-//includes
+// includes
+#include "menu/IMenu.hpp"
 #include "pros/misc.hpp"
+#include "pvegas/io/IController.hpp"
+#include "pvegas/robot/Robot.hpp"
+#include "pvegas/rtos/IClock.hpp"
+#include "pvegas/rtos/IDelayer.hpp"
 #include <memory>
 
-namespace pvegas{
-    class MatchController{
-        std::shared_ptr<pros::Controller> controller();
-    };
-}
+namespace pvegas {
+class MatchController {
+private:
+  static constexpr uint32_t MENU_DELAY{10};
+
+  std::unique_ptr<menu::IMenu> m_menu{};
+
+  std::shared_ptr<rtos::IClock> m_clock{};
+
+  std::unique_ptr<rtos::IDelayer> m_delayer{};
+
+  std::shared_ptr<io::IController> m_controller{};
+
+  std::shared_ptr<robot::Robot> robot{};
+
+public:
+  MatchController(std::unique_ptr<menu::IMenu> &new_menu,
+                  std::shared_ptr<rtos::IClock> &clock,
+                  std::unique_ptr<rtos::IDelayer> &delayer);
+
+  void init(bool fast_init);
+
+  void disabled();
+
+  void competitionInit();
+
+  void autonomous();
+
+  void operatorControl();
+};
+} // namespace pvegas
 #endif
