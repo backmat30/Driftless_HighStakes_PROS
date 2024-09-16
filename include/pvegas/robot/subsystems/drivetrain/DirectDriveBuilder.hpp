5a65a699f3dd3542d7fcff8bf11da1c7661a215b
backmat30 <matthewbackman@outlook.com>
2024-09-16 18:14:15 -0500

    adds comments to methods in the drivetrain factory

--- include/pvegas/robot/subsystems/drivetrain/DirectDriveBuilder.hpp
+++ include/pvegas/robot/subsystems/drivetrain/DirectDriveBuilder.hpp
@@ -23,16 +23,22 @@ private:
   double m_drive_radius{};
 
 public:
+  // Adds a designated motor to the left side of the robot
   DirectDriveBuilder* withLeftMotor(std::unique_ptr<io::IMotor>& motor);
 
+  // Adds a designated motor to the right side of the robot
   DirectDriveBuilder* withRightMotor(std::unique_ptr<io::IMotor>& motor);
 
+  // Designates the ratio between robot velocity and motor voltage
   DirectDriveBuilder* withVelocityToVoltage(double velocity_to_voltage);
 
+  // Designates the radius of the drive wheels
   DirectDriveBuilder* withWheelRadius(double wheel_radius);
 
+  // Designates the radius of the robot
   DirectDriveBuilder* withDriveRadius(double drive_radius);
 
+  //final call in the chain, uses the newly defined data to construct a drive train
   std::unique_ptr<IDrivetrain> build();
 };
 } // namespace drivetrain
