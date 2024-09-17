cc69f921feb57c62dc0d4d4eacb20d780ad60e2b
backmat30 <matthewbackman@outlook.com>
2024-09-17 11:44:59 -0500

    Adds a default config to set up the robot

---
+++ include/pvegas/config/DefaultConfig.hpp
@@ -0,0 +1,68 @@
+#ifndef __DEFAULT_CONFIG_HPP__
+#define __DEFAULT_CONFIG_HPP__
+
+#include "pros/abstract_motor.hpp"
+#include "pros/motors.h"
+#include "pvegas/pros_adapters/ProsV5Motor.hpp"
+#include "pros_adapters/ProsClock.hpp"
+#include "pvegas/control/ControlSystem.hpp"
+#include "pvegas/io/IController.hpp"
+#include "pvegas/pros_adapters/ProsController.hpp"
+#include "pvegas/pros_adapters/ProsDelayer.hpp"
+#include "pvegas/pros_adapters/ProsMutex.hpp"
+#include "pvegas/pros_adapters/ProsTask.hpp"
+#include "pvegas/robot/Robot.hpp"
+#include "pvegas/robot/subsystems/drivetrain/DirectDriveBuilder.hpp"
+#include "pvegas/rtos/IDelayer.hpp"
+#include "pvegas/rtos/IMutex.hpp"
+#include "pvegas/rtos/ITask.hpp"
+#include "rtos/IClock.hpp"
+#include "pvegas/config/IConfig.hpp"
+#include <memory>
+namespace pvegas {
+namespace config {
+class DefaultConfig : public IConfig {
+private:
+  static constexpr char CONFIG_NAME[]{"DEFAULT CONFIG"};
+
+  // -----PORT NUMBERS-----
+  // DRIVE MOTORS
+  // first left drive motor
+  static constexpr int8_t DRIVE_LEFT_MOTOR_1{-1};
+  // second left drive motor
+  static constexpr int8_t DRIVE_LEFT_MOTOR_2{-1};
+  // third left drive motor
+  static constexpr int8_t DRIVE_LEFT_MOTOR_3{-1};
+  // fourth left drive motor
+  static constexpr int8_t DRIVE_LEFT_MOTOR_4{-1};
+  // first right drive motor
+  static constexpr int8_t DRIVE_RIGHT_MOTOR_1{-1};
+  // second right drive motor
+  static constexpr int8_t DRIVE_RIGHT_MOTOR_2{-1};
+  // third right drive motor
+  static constexpr int8_t DRIVE_RIGHT_MOTOR_3{-1};
+  // fourth right drive motor
+  static constexpr int8_t DRIVE_RIGHT_MOTOR_4{-1};
+
+  // -----MISC VALUES-----
+  // drive gearset
+  static constexpr pros::MotorGearset DRIVE_GEARSET{pros::E_MOTOR_GEAR_BLUE};
+  // drive ratio of motor voltage to velocity
+  static constexpr double DRIVE_VELOCITY_TO_VOLTAGE{1.0};
+  // radius of the robot
+  static constexpr double ROBOT_RADIUS{7.25};
+  // radius of the drive wheels
+  static constexpr double DRIVE_WHEEL_RADIUS{1.25};
+
+public:
+  std::string getName() override;
+
+  std::shared_ptr<control::ControlSystem> buildControlSystem() override;
+
+  std::shared_ptr<io::IController> buildController() override;
+
+  std::shared_ptr<robot::Robot> buildRobot() override;
+};
+} // namespace config
+} // namespace pvegas
+#endif
---
+++ src/pvegas/config/DefaultConfig.cpp
@@ -0,0 +1,103 @@
+#include "pvegas/config/DefaultConfig.hpp"
+#include "robot/subsystems/drivetrain/DrivetrainSubsystem.hpp"
+#include "robot/subsystems/drivetrain/IDriveTrain.hpp"
+#include <memory>
+
+namespace pvegas {
+namespace config {
+std::string DefaultConfig::getName() { return CONFIG_NAME; }
+
+std::shared_ptr<control::ControlSystem> DefaultConfig::buildControlSystem() {
+  // creates a new ControlSystem object through the copy constructor
+  std::shared_ptr<control::ControlSystem> control_system{
+      std::make_shared<control::ControlSystem>()};
+
+  // TODO - add controls as needed
+
+  return control_system;
+}
+
+std::shared_ptr<io::IController> DefaultConfig::buildController() {
+  // creates a default pros controller
+  std::unique_ptr<pros::Controller> pros_controller{
+      std::make_unique<pros::Controller>(pros::E_CONTROLLER_MASTER)};
+  // adapts the pros controller to work as an IController object
+  std::shared_ptr<io::IController> adapted_controller{
+      std::make_shared<pros_adapters::ProsController>(pros_controller)};
+  // send back a new adapted controller
+  return adapted_controller;
+}
+
+std::shared_ptr<robot::Robot> DefaultConfig::buildRobot() {
+  // create the robot
+  std::shared_ptr<robot::Robot> robot{std::make_shared<robot::Robot>()};
+
+  // DRIVE TRAIN SETUP
+  // creates the factory used to build the drivetrain
+  robot::subsystems::drivetrain::DirectDriveBuilder drive_factory{};
+
+  // Objects needed for drivetrain
+  // pros objects
+  // left motors
+  std::unique_ptr<pros::Motor> left_temp_motor_1{
+      std::make_unique<pros::Motor>(DRIVE_LEFT_MOTOR_1, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> left_temp_motor_2{
+      std::make_unique<pros::Motor>(DRIVE_LEFT_MOTOR_2, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> left_temp_motor_3{
+      std::make_unique<pros::Motor>(DRIVE_LEFT_MOTOR_3, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> left_temp_motor_4{
+      std::make_unique<pros::Motor>(DRIVE_LEFT_MOTOR_4, DRIVE_GEARSET)};
+  // right motors
+  std::unique_ptr<pros::Motor> right_temp_motor_1{
+      std::make_unique<pros::Motor>(DRIVE_RIGHT_MOTOR_1, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> right_temp_motor_2{
+      std::make_unique<pros::Motor>(DRIVE_RIGHT_MOTOR_2, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> right_temp_motor_3{
+      std::make_unique<pros::Motor>(DRIVE_RIGHT_MOTOR_3, DRIVE_GEARSET)};
+  std::unique_ptr<pros::Motor> right_temp_motor_4{
+      std::make_unique<pros::Motor>(DRIVE_RIGHT_MOTOR_4, DRIVE_GEARSET)};
+
+  // pros adapters
+  // left motors
+  std::unique_ptr<io::IMotor> left_motor_1{
+      std::make_unique<pros_adapters::ProsV5Motor>(left_temp_motor_1)};
+  std::unique_ptr<io::IMotor> left_motor_2{
+      std::make_unique<pros_adapters::ProsV5Motor>(left_temp_motor_2)};
+  std::unique_ptr<io::IMotor> left_motor_3{
+      std::make_unique<pros_adapters::ProsV5Motor>(left_temp_motor_3)};
+  std::unique_ptr<io::IMotor> left_motor_4{
+      std::make_unique<pros_adapters::ProsV5Motor>(left_temp_motor_4)};
+  // right motors
+  std::unique_ptr<io::IMotor> right_motor_1{
+      std::make_unique<pros_adapters::ProsV5Motor>(right_temp_motor_1)};
+  std::unique_ptr<io::IMotor> right_motor_2{
+      std::make_unique<pros_adapters::ProsV5Motor>(right_temp_motor_2)};
+  std::unique_ptr<io::IMotor> right_motor_3{
+      std::make_unique<pros_adapters::ProsV5Motor>(right_temp_motor_3)};
+  std::unique_ptr<io::IMotor> right_motor_4{
+      std::make_unique<pros_adapters::ProsV5Motor>(right_temp_motor_4)};
+
+  // assembling the drive train
+  std::unique_ptr<robot::subsystems::drivetrain::IDrivetrain> drivetrain{
+    //call the factory and add all necessary items for the drivetrain
+      drive_factory.withLeftMotor(left_motor_1)
+          ->withLeftMotor(left_motor_2)
+          ->withLeftMotor(left_motor_3)
+          ->withLeftMotor(left_motor_4)
+          ->withRightMotor(right_motor_1)
+          ->withRightMotor(right_motor_2)
+          ->withRightMotor(right_motor_3)
+          ->withRightMotor(right_motor_4)
+          ->withVelocityToVoltage(DRIVE_VELOCITY_TO_VOLTAGE)
+          ->withDriveRadius(ROBOT_RADIUS)
+          ->withWheelRadius(DRIVE_WHEEL_RADIUS)
+          ->build()};
+  // create the subsystem for the drivetrain
+  std::unique_ptr<robot::ASubsystem> drivetrain_subsystem{
+      std::make_unique<robot::subsystems::drivetrain::DrivetrainSubsystem>(
+          drivetrain)};
+  // add the new subsystem to the robot
+  robot->addSubsystem(drivetrain_subsystem);
+}
+} // namespace config
+} // namespace pvegas
