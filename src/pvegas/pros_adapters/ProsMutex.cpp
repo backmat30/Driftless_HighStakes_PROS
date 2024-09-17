2e009b08b15a61ecbfec84cc02fc26880bab3c84
backmat30 <matthewbackman@outlook.com>
2024-09-16 22:40:10 -0500

    Adds a mutex interface and adapters for the pros mutex

---
+++ src/pvegas/pros_adapters/ProsMutex.cpp
@@ -0,0 +1,10 @@
+#include "pvegas/pros_adapters/ProsMutex.hpp"
+
+namespace pvegas {
+namespace pros_adapters {
+
+void ProsMutex::take() { mutex.take(); }
+
+void ProsMutex::give() { mutex.give(); }
+} // namespace pros_adapters
+} // namespace pvegas
