31644c2cf8d94a16624b70f1ae13cca010e4146d
backmat30 <matthewbackman@outlook.com>
2024-09-17 10:21:36 -0500

    Added Task interface and an adapter to the pros task

---
+++ src/pvegas/pros_adapters/ProsTask.cpp
@@ -0,0 +1,40 @@
+#include "pvegas/pros_adapters/ProsTask.hpp"
+#include "pros/rtos.hpp"
+#include <memory>
+
+namespace pvegas {
+namespace pros_adapters {
+void ProsTask::start(void (*function)(void *), void *params) {
+  // defines the task
+  task = std::make_unique<pros::Task>(function, params);
+}
+
+void ProsTask::remove() {
+    // removes the task from the system if it exists
+  if (task) {
+    task->remove();
+  }
+}
+
+void ProsTask::suspend() {
+    // attempts to suspend the task if it exists
+  if (task) {
+    task->suspend();
+  }
+}
+
+void ProsTask::resume() {
+    // attempts to resume the task if it exists
+  if (task) {
+    task->resume();
+  }
+}
+
+void ProsTask::join() {
+    // calls the join function if the task exists
+  if (task) {
+    task->join();
+  }
+}
+} // namespace pros_adapters
+} // namespace pvegas
