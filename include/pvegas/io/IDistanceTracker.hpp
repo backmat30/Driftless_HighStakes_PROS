63e39ffc6286183c9a02ac577767e073128cbc10
backmat30 <matthewbackman@outlook.com>
2024-10-14 18:05:32 -0500

    Added interfaces for odometry-related hardware

---
+++ include/pvegas/io/IDistanceTracker.hpp
@@ -0,0 +1,25 @@
+#ifndef __I_DISTANCE_TRACKER_HPP__
+#define __I_DISTANCE_TRACKER_HPP__
+
+namespace pvegas {
+namespace io {
+class IDistanceTracker {
+ public:
+  // destroyer
+  virtual ~IDistanceTracker() = default;
+
+  // initialize the distance tracker
+  virtual void init() = 0;
+
+  // reset the distance tracker
+  virtual void reset() = 0;
+
+  // get the distance recorded by the distance tracker
+  virtual double getDistance() = 0;
+
+  // sets the distance tracked by the sensor to a new value
+  virtual void setDistance(double distance) = 0;
+};
+}  // namespace io
+}  // namespace pvegas
+#endif
---
+++ include/pvegas/io/IRotationSensor.hpp
@@ -0,0 +1,28 @@
+#ifndef __I_ROTATION_SENSOR_HPP__
+#define __I_ROTATION_SENSOR_HPP__
+
+namespace pvegas {
+namespace io {
+class IRotationSensor {
+ public:
+  // destroyer
+  virtual ~IRotationSensor() = default;
+
+  // initializes the rotational sensor
+  virtual void init() = 0;
+
+  // resets the rotational sensor
+  virtual void reset() = 0;
+
+  // retrieves the rotations done by the sensor
+  virtual double getRotations() = 0;
+
+  // sets the rotations to a new value
+  virtual void setRotations(double rotations) = 0;
+
+  // gets the angle the rotational sensor is at
+  virtual double getAngle() = 0;
+};
+}  // namespace io
+}  // namespace pvegas
+#endif
