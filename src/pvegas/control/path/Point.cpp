aae198a7410b751bf393a047d1223ae5d41452ca
backmat30 <matthewbackman@outlook.com>
2024-10-15 19:09:53 -0500

    added point and position objects for odometry utility

---
+++ src/pvegas/control/path/Point.cpp
@@ -0,0 +1,53 @@
+#include "pvegas/control/path/Point.hpp"
+
+namespace pvegas {
+namespace control {
+namespace path {
+Point::Point(double x, double y) : m_x{x}, m_y{y} {}
+
+void Point::setX(double x) { m_x = x; }
+
+void Point::setY(double y) { m_y = y; }
+
+double Point::getX() { return m_x; }
+
+double Point::getY() { return m_y; }
+
+Point Point::operator+(const Point& rhs) {
+  return Point{m_x + rhs.m_x, m_y + rhs.m_y};
+}
+
+Point Point::operator-(const Point& rhs) {
+  return Point{m_x - rhs.m_x, m_y - rhs.m_y};
+}
+
+Point Point::operator*(double rhs) { return Point{m_x * rhs, m_y * rhs}; }
+
+Point Point::operator/(double rhs) { return Point{m_x / rhs, m_y / rhs}; }
+
+Point& Point::operator+=(const Point& rhs) {
+  m_x += rhs.m_x;
+  m_y += rhs.m_y;
+  return *this;
+}
+
+Point& Point::operator-=(const Point& rhs) {
+  m_x -= rhs.m_x;
+  m_y -= rhs.m_y;
+  return *this;
+}
+
+Point& Point::operator*=(double rhs) {
+  m_x *= rhs;
+  m_y *= rhs;
+  return *this;
+}
+
+Point& Point::operator/=(double rhs) {
+  m_x /= rhs;
+  m_y /= rhs;
+  return *this;
+}
+}  // namespace path
+}  // namespace control
+}  // namespace pvegas
