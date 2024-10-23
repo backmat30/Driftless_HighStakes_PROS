1f47e3fb448d5c078e87a4927ca3267b5383b761
backmat30 <matthewbackman@outlook.com>
2024-10-22 20:42:27 -0500

    Added interfaces for basic motion control

---
+++ include/pvegas/control/motion/ETurnDirection.hpp
@@ -0,0 +1,11 @@
+#ifndef __E_TURN_DIRECTION_HPP__
+#define __E_TURN_DIRECTION_HPP__
+
+namespace pvegas {
+namespace control {
+namespace motion {
+enum class ETurnDirection { AUTO, CLOCKWISE, COUNTERCLOCKWISE };
+}
+}  // namespace control
+}  // namespace pvegas
+#endif
---
+++ include/pvegas/control/motion/ITurn.hpp
@@ -0,0 +1,49 @@
+#ifndef __I_TURN_HPP__
+#define __I_TURN_HPP__
+
+#include <memory>
+
+#include "pvegas/control/Point.hpp"
+#include "pvegas/control/motion/ETurnDirection.hpp"
+#include "pvegas/robot/Robot.hpp"
+
+namespace pvegas {
+namespace control {
+namespace motion {
+class ITurn {
+ public:
+  // destroyer
+  virtual ~ITurn() = default;
+
+  // initialize the control
+  virtual void init() = 0;
+
+  // run the control
+  virtual void run() = 0;
+
+  // pause the control
+  virtual void pause() = 0;
+
+  // resume the control
+  virtual void resume() = 0;
+
+  // tell the robot to turn in to a given angle
+  virtual void turnToAngle(std::shared_ptr<pvegas::robot::Robot>& robot,
+                           double velocity, double theta,
+                           ETurnDirection direction = ETurnDirection::AUTO) = 0;
+
+  // tell the robot to turn towards a point on the field
+  virtual void turnToPoint(std::shared_ptr<pvegas::robot::Robot>& robot,
+                           double velocity, Point point,
+                           ETurnDirection direction = ETurnDirection::AUTO) = 0;
+
+  // set the max velocity
+  virtual void setVelocity(double velocity) = 0;
+
+  // determines if the robot has reached the target
+  virtual bool targetReached() = 0;
+};
+}  // namespace motion
+}  // namespace control
+}  // namespace pvegas
+#endif
