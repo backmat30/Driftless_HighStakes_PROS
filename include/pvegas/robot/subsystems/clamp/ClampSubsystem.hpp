b790268210923847b4a3d95bd11c0d6890d1d80f
backmat30 <matthewbackman@outlook.com>
2024-11-04 12:32:56 -0600

    added the clamp subsystem

---
+++ include/pvegas/robot/subsystems/clamp/ClampSubsystem.hpp
@@ -0,0 +1,47 @@
+#ifndef __CLAMP_SUBSYSTEM_HPP__
+#define __CLAMP_SUBSYSTEM_HPP__
+
+#include <memory>
+
+#include "pvegas/robot/subsystems/ASubsystem.hpp"
+#include "pvegas/robot/subsystems/clamp/IClamp.hpp"
+
+namespace pvegas {
+namespace robot {
+namespace subsystems {
+namespace clamp {
+class ClampSubsystem : public ASubsystem {
+ private:
+  // the name of the subsystem
+  static constexpr char SUBSYSTEM_NAME[]{"CLAMP"};
+
+  // the name of the set state command
+  static constexpr char SET_STATE_COMMAND_NAME[]{"SET STATE"};
+
+  // the name of the clamp state
+  static constexpr char GET_STATE_STATE_NAME[]{"GET STATE"};
+
+  // the clamp used by the subsystem
+  std::unique_ptr<IClamp> m_clamp{};
+
+ public:
+  // constructs a new clamp subsystem object
+  ClampSubsystem(std::unique_ptr<IClamp>& clamp);
+
+  // initializes the subsystem
+  void init() override;
+
+  // runs the subsystem
+  void run() override;
+
+  // execute a given command
+  void command(std::string command_name, va_list& args) override;
+
+  // get a state of the subsystem
+  void* state(std::string state_name) override;
+};
+}  // namespace clamp
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace pvegas
+#endif
---
+++ src/pvegas/robot/subsystems/clamp/ClampSubsystem.cpp
@@ -0,0 +1,36 @@
+#include "pvegas/robot/subsystems/clamp/ClampSubsystem.hpp"
+
+namespace pvegas {
+namespace robot {
+namespace subsystems {
+namespace clamp {
+ClampSubsystem::ClampSubsystem(std::unique_ptr<IClamp>& clamp)
+    : m_clamp{std::move(clamp)} {}
+
+void ClampSubsystem::init() {
+
+}
+
+void ClampSubsystem::run() {
+
+}
+
+void ClampSubsystem::command(std::string command_name, va_list& args) {
+  if(command_name == SET_STATE_COMMAND_NAME) {
+    bool state{va_arg(args, bool)};
+    m_clamp->setState(state);
+  }
+}
+
+void* ClampSubsystem::state(std::string state_name) {
+  void* result{nullptr};
+  if(state_name == GET_STATE_STATE_NAME) {
+    result = new bool{m_clamp->getState()};
+  }
+
+  return result;
+}
+}  // namespace clamp
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace pvegas
