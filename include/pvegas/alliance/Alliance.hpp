5d0cec762bef329f2c0aa1aa2e0422a284f0473a
backmat30 <matthewbackman@outlook.com>
2024-11-05 21:12:37 -0600

    Added a struct for alliance-related information

---
+++ include/pvegas/alliance/Alliance.hpp
@@ -0,0 +1,17 @@
+#ifndef __ALLIANCE_HPP__
+#define __ALLIANCE_HPP__
+
+#include <string>
+
+namespace pvegas {
+namespace alliance {
+struct Alliance {
+  // name of the alliance
+  std::string name{};
+
+  // min and max hue for alliance ring hue
+  double hue_range[2]{};
+};
+}  // namespace alliance
+}  // namespace pvegas
+#endif
