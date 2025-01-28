00066c2d5a5e78466ed81f345fc51e18c245372e
backmat30 <matthewbackman@outlook.com>
2025-01-28 13:46:00 -0600

    Refactor ArmOperator class to use ESubsystemCommand and ESubsystemState enums for improved type safety and clarity

--- include/driftless/op_control/arm/ArmOperator.hpp
+++ include/driftless/op_control/arm/ArmOperator.hpp
@@ -7,6 +7,8 @@
 #include "driftless/op_control/arm/EArmControlMode.hpp"
 #include "driftless/profiles/IProfile.hpp"
 #include "driftless/robot/Robot.hpp"
+#include "driftless/robot/subsystems/ESubsystemCommand.hpp"
+#include "driftless/robot/subsystems/ESubsystemState.hpp"
 
 namespace driftless {
 namespace op_control {
@@ -18,81 +20,6 @@ class ArmOperator {
 
   // name of the red alliance
   static constexpr char RED_ALLIANCE_NAME[]{"RED"};
-
-  // name of the arm subsystem
-  static constexpr char ARM_SUBSYSTEM_NAME[]{"ARM"};
-
-  // name of the ring sort subsystem
-  static constexpr char RING_SORT_SUBSYSTEM_NAME[]{"RING SORT"};
-
-  // command to go to the neutral position
-  static constexpr char GO_NEUTRAL_COMMAND_NAME[]{"GO NEUTRAL"};
-
-  // command to go to the load position
-  static constexpr char GO_LOAD_COMMAND_NAME[]{"GO LOAD"};
-
-  // command to go to the ready position
-  static constexpr char GO_READY_COMMAND_NAME[]{"GO READY"};
-
-  // command to go to the score position
-  static constexpr char GO_SCORE_COMMAND_NAME[]{"GO SCORE"};
-
-  // command to go to the rush position
-  static constexpr char GO_RUSH_COMMAND_NAME[]{"GO RUSH"};
-
-  // command to go to the alliance stake position
-  static constexpr char GO_ALLIANCE_STAKE_COMMAND_NAME[]{"GO ALLIANCE STAKE"};
-
-  // command to go to the previous position
-  static constexpr char GO_PREVIOUS_COMMAND_NAME[]{"GO PREVIOUS"};
-
-  // command to calibrate the arm
-  static constexpr char CALIBRATE_COMMAND_NAME[]{"CALIBRATE"};
-
-  // state determining if the arm is at neutral position
-  static constexpr char IS_NEUTRAL_STATE_NAME[]{"IS NEUTRAL"};
-
-  // state determining if the arm is moving to the neutral position
-  static constexpr char IS_GOING_NEUTRAL_STATE_NAME[]{"IS GOING NEUTRAL"};
-
-  // state determining if the arm is at load position
-  static constexpr char IS_LOAD_STATE_NAME[]{"IS LOAD"};
-
-  // state determining if the arm is going to the load position
-  static constexpr char IS_GOING_LOAD_STATE_NAME[]{"IS GOING LOAD"};
-
-  // state determining if the arm is at the ready position
-  static constexpr char IS_READY_STATE_NAME[]{"IS READY"};
-
-  // state determining if the arm is going to the ready position
-  static constexpr char IS_GOING_READY_STATE_NAME[]{"IS GOING READY"};
-
-  // state determining if the arm is at score position
-  static constexpr char IS_SCORE_STATE_NAME[]{"IS SCORE"};
-
-  // state determining if the arm is going to the score position
-  static constexpr char IS_GOING_SCORE_STATE_NAME[]{"IS GOING SCORE"};
-
-  // state determining if the arm is in the rush position
-  static constexpr char IS_RUSH_STATE_NAME[]{"IS RUSH"};
-
-  // state determining if the arm is going to the rush position
-  static constexpr char IS_GOING_RUSH_STATE_NAME[]{"IS GOING RUSH"};
-
-  // state determining if the arm is at the alliance stake position
-  static constexpr char IS_ALLIANCE_STAKE_STATE_NAME[]{"IS ALLIANCE STAKE"};
-
-  // state determining if the arm is going to the alliance stake position
-  static constexpr char IS_GOING_ALLIANCE_STAKE_STATE_NAME[]{
-      "IS GOING ALLIANCE STAKE"};
-
-  // state determining if there is a ring in the loading zone
-  static constexpr char HAS_RING_STATE_NAME[]{"HAS RING"};
-
-  // state determining the color of the ring in the loading zone, if applicable
-  static constexpr char GET_HUE_STATE_NAME[]{"GET HUE"};
-
-  static constexpr char GET_RGB_STATE_NAME[]{"GET RGB"};
 
   // the controller used
   std::shared_ptr<driftless::io::IController> m_controller{};
--- src/driftless/op_control/arm/ArmOperator.cpp
+++ src/driftless/op_control/arm/ArmOperator.cpp
@@ -5,11 +5,12 @@ namespace op_control {
 namespace arm {
 bool ArmOperator::hasAllianceRing(
     const std::shared_ptr<alliance::IAlliance>& alliance) {
-  bool has_ring{*static_cast<bool*>(
-      m_robot->getState(RING_SORT_SUBSYSTEM_NAME, HAS_RING_STATE_NAME))};
+  bool has_ring{*static_cast<bool*>(m_robot->getState(
+      robot::subsystems::ESubsystem::RING_SORT,
+      robot::subsystems::ESubsystemState::RING_SORT_HAS_RING))};
 
   io::RGBValue ring_rgb{*static_cast<io::RGBValue*>(
-      m_robot->getState(RING_SORT_SUBSYSTEM_NAME, GET_RGB_STATE_NAME))};
+      m_robot->getState(robot::subsystems::ESubsystem::RING_SORT, robot::subsystems::ESubsystemState::RING_SORT_GET_RGB))};
 
   bool has_alliance_ring{};
 
@@ -28,10 +29,10 @@ bool ArmOperator::hasAllianceRing(
 bool ArmOperator::hasOpposingRing(
     const std::shared_ptr<alliance::IAlliance>& alliance) {
   bool has_ring{*static_cast<bool*>(
-      m_robot->getState(RING_SORT_SUBSYSTEM_NAME, HAS_RING_STATE_NAME))};
+      m_robot->getState(robot::subsystems::ESubsystem::RING_SORT, robot::subsystems::ESubsystemState::RING_SORT_HAS_RING))};
 
   io::RGBValue ring_rgb{*static_cast<io::RGBValue*>(
-      m_robot->getState(RING_SORT_SUBSYSTEM_NAME, GET_RGB_STATE_NAME))};
+      m_robot->getState(robot::subsystems::ESubsystem::RING_SORT, robot::subsystems::ESubsystemState::RING_SORT_GET_RGB))};
 
   bool has_opposing_ring{};
 
@@ -58,22 +59,22 @@ void ArmOperator::updateSplitToggle(
 
   if (go_neutral && !go_load && !go_ready && !go_score) {
     bool at_load{*static_cast<bool*>(
-        m_robot->getState(ARM_SUBSYSTEM_NAME, IS_LOAD_STATE_NAME))};
+        m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_LOAD))};
     if (!(hasAllianceRing(alliance) && at_load)) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_NEUTRAL_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_NEUTRAL);
     }
 
   } else if (!go_neutral && go_load && !go_ready && !go_score) {
-    m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_LOAD_COMMAND_NAME);
+    m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_LOAD);
 
   } else if (!go_neutral && !go_load && go_ready && !go_score) {
-    m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_READY_COMMAND_NAME);
+    m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_READY);
   } else if (!go_neutral && !go_load && go_score) {
     bool is_ready{*static_cast<bool*>(
-        m_robot->getState(ARM_SUBSYSTEM_NAME, IS_READY_STATE_NAME))};
+        m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_READY))};
 
     if (is_ready) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_SCORE_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_SCORE);
     }
   }
 }
@@ -88,50 +89,50 @@ void ArmOperator::updateSmartToggle(
   bool go_alliance_stake{m_controller->getNewDigital(alliance_stake)};
 
   void* is_neutral_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_NEUTRAL_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_NEUTRAL)};
   bool is_neutral{is_neutral_state != nullptr &&
                   *static_cast<bool*>(is_neutral_state)};
 
   void* is_going_neutral_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_GOING_NEUTRAL_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_GOING_NEUTRAL)};
   bool is_going_neutral{is_going_neutral_state != nullptr &&
                         *static_cast<bool*>(is_going_neutral_state)};
 
   void* is_load_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_LOAD_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_LOAD)};
   bool is_load{is_load_state != nullptr && *static_cast<bool*>(is_load_state)};
 
   void* is_going_load_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_GOING_LOAD_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_GOING_LOAD)};
   bool is_going_load{is_going_load_state != nullptr &&
                      *static_cast<bool*>(is_going_load_state)};
 
   void* is_ready_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_READY_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_READY)};
   bool is_ready{is_ready_state != nullptr &&
                 *static_cast<bool*>(is_ready_state)};
 
   void* is_going_ready_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_GOING_READY_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_GOING_READY)};
   bool is_going_ready{is_going_ready_state != nullptr &&
                       *static_cast<bool*>(is_going_ready_state)};
 
   void* is_score_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_SCORE_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_SCORE)};
   bool is_score{is_score_state != nullptr &&
                 *static_cast<bool*>(is_score_state)};
 
   void* is_going_score_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_GOING_SCORE_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_GOING_SCORE)};
   bool is_going_score{is_going_score_state != nullptr &&
                       *static_cast<bool*>(is_going_score_state)};
 
   void* is_rush_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_RUSH_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_RUSH)};
   bool is_rush{is_rush_state != nullptr && *static_cast<bool*>(is_rush_state)};
 
   void* is_going_rush_state{
-      m_robot->getState(ARM_SUBSYSTEM_NAME, IS_GOING_RUSH_STATE_NAME)};
+      m_robot->getState(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemState::ARM_IS_GOING_RUSH)};
   bool is_going_rush{is_going_rush_state != nullptr &&
                      *static_cast<bool*>(is_going_rush_state)};
 
@@ -142,38 +143,38 @@ void ArmOperator::updateSmartToggle(
 
   /*if (has_opposing_ring) {
     if (is_load) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_NEUTRAL_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_NEUTRAL);
     }
   } else {*/
   if (next_position && !go_rush && !calibrate_arm && !go_alliance_stake) {
     if (is_neutral) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_LOAD_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_LOAD);
     } else if (is_load && has_alliance_ring) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_READY_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_READY);
     } else if (is_ready) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_SCORE_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_SCORE);
     } else if (is_rush) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_LOAD_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_LOAD);
     } else if (is_going_neutral || is_going_load || is_going_ready ||
                is_going_score || is_going_rush) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_PREVIOUS_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_PREVIOUS);
     } else {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_NEUTRAL_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_NEUTRAL);
     }
   } else if (!next_position && go_rush && !calibrate_arm &&
              !go_alliance_stake) {
     if (is_rush) {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_LOAD_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_LOAD);
     } else {
-      m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_RUSH_COMMAND_NAME);
+      m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_RUSH);
     }
   } else if (!next_position && !go_rush && !calibrate_arm &&
              go_alliance_stake) {
-    m_robot->sendCommand(ARM_SUBSYSTEM_NAME, GO_ALLIANCE_STAKE_COMMAND_NAME);
+    m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_GO_ALLIANCE_STAKE);
   }
   //}
   if (!next_position && !go_rush && calibrate_arm && !go_alliance_stake) {
-    m_robot->sendCommand(ARM_SUBSYSTEM_NAME, CALIBRATE_COMMAND_NAME);
+    m_robot->sendCommand(robot::subsystems::ESubsystem::ARM, robot::subsystems::ESubsystemCommand::ARM_CALIBRATE);
   }
 }
 
