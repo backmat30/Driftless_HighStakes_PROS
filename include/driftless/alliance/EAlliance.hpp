6fe2b03f927fa7ee5da2e54410cc9762b6cf2748
backmat30 <matthewbackman@outlook.com>
2025-01-28 15:10:37 -0600

    Add EAlliance enum for alliance color representation

---
+++ include/driftless/alliance/EAlliance.hpp
@@ -0,0 +1,9 @@
+#ifndef __E_ALLIANCE_HPP__
+#define __E_ALLIANCE_HPP__
+
+namespace driftless {
+namespace alliance {
+enum class EAlliance { BLUE, RED };
+}  // namespace alliance
+}  // namespace driftless
+#endif
