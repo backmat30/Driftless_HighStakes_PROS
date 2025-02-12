f19eb0bb653e44e0fc8e5076e100771d876b6009
backmat30 <matthewbackman@outlook.com>
2025-02-11 19:40:42 -0600

    Limits the linear acceleration to reduce inconsistency

--- src/driftless/control/motion/PIDDriveStraight.cpp
+++ src/driftless/control/motion/PIDDriveStraight.cpp
@@ -46,6 +46,15 @@ void PIDDriveStraight::updateVelocity(double distance, double theta) {
   if (std::abs(linear_control) > m_max_velocity) {
     linear_control *= m_max_velocity / std::abs(linear_control);
   }
+
+  double current_velocity{getVelocity()};
+
+  // limits acceleration to half of the max velocity per second
+  if (std::abs(linear_control) >= std::abs(current_velocity) + 36.0) {
+    linear_control *= (std::abs(current_velocity) + 36.0) /
+                      std::abs(linear_control);
+  }
+
   double left_velocity{linear_control - angular_control};
   double right_velocity{linear_control + angular_control};
 
--- src/driftless/control/motion/PIDGoToPoint.cpp
+++ src/driftless/control/motion/PIDGoToPoint.cpp
@@ -39,6 +39,16 @@ void PIDGoToPoint::updateVelocity(double distance, double target_angle,
     linear_control *= m_max_velocity / std::abs(linear_control);
   }
 
+  double current_velocity{getVelocity()};
+
+  // limits acceleration to 36.0 inches per second
+
+  // @todo add acceleration limit as a member variable instead of magic number
+  if (std::abs(linear_control) >= std::abs(current_velocity) + 36.0) {
+    linear_control *= (std::abs(current_velocity) + 36.0) /
+                      std::abs(linear_control);
+  }
+
   double angular_offset{bindRadians(target_angle - theta)};
   double angular_error{};
   if (angular_offset < 0) {
