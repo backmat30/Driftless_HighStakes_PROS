12465f10d3fd1cb48db50507fc957cc8197f21f8
backmat30 <matthewbackman@outlook.com>
2025-03-17 11:33:58 -0500

    Added logic to unjam the intake if torque gets too high

--- include/driftless/robot/subsystems/intake/DirectIntake.hpp
+++ include/driftless/robot/subsystems/intake/DirectIntake.hpp
@@ -5,6 +5,10 @@
 
 #include "driftless/hal/MotorGroup.hpp"
 #include "driftless/robot/subsystems/intake/IIntake.hpp"
+#include "driftless/rtos/IClock.hpp"
+#include "driftless/rtos/IDelayer.hpp"
+#include "driftless/rtos/IMutex.hpp"
+#include "driftless/rtos/ITask.hpp"
 
 /// @brief The namespace for driftless library code
 /// @author Matthew Backman
@@ -26,8 +30,34 @@ namespace intake {
 /// @author Matthew Backman
 class DirectIntake : public IIntake {
  private:
+  static constexpr uint8_t TASK_DELAY{10};
+
+  /// @brief Periodically updates the designated intake
+  /// @param params __void*__ The intake to update
+  static void taskLoop(void* params);
+
   // group of motors used to run the intake
   driftless::hal::MotorGroup m_motors{};
+
+  std::unique_ptr<rtos::IClock> m_clock{};
+
+  std::unique_ptr<rtos::IDelayer> m_delayer{};
+
+  std::unique_ptr<rtos::IMutex> m_mutex{};
+
+  std::unique_ptr<rtos::ITask> m_task{};
+
+  double target_voltage{};
+
+  uint32_t latest_jam_time{};
+
+  bool jammed{};
+
+  /// @brief Updates the intake
+  void taskUpdate();
+
+  /// @brief Unjams the intake
+  void unjam();
 
  public:
   /// @brief Initializes the intake
@@ -43,6 +73,22 @@ class DirectIntake : public IIntake {
   /// @brief Sets the motors
   /// @param motors __driftless::hal::MotorGroup&__ The motors to set
   void setMotors(driftless::hal::MotorGroup& motors);
+
+  /// @brief Sets the clock used by the intake
+  /// @param clock __const std::unique_ptr<rtos::IClock>&__ The clock to use
+  void setClock(const std::unique_ptr<rtos::IClock>& clock);
+
+  /// @brief Sets the delayer used by the intake
+  /// @param delayer __const std::unique_ptr<rtos::IDelayer>&__ The delayer to use
+  void setDelayer(const std::unique_ptr<rtos::IDelayer>& delayer);
+
+  /// @brief Sets the mutex used by the intake
+  /// @param mutex __std::unique_ptr<rtos::IMutex>&__ The mutex to use
+  void setMutex(std::unique_ptr<rtos::IMutex>& mutex);
+
+  /// @brief Sets the task used by the intake
+  /// @param task __std::unique_ptr<rtos::ITask>&__ The task to use
+  void setTask(std::unique_ptr<rtos::ITask>& task);
 };
 }  // namespace intake
 }  // namespace subsystems
--- src/driftless/robot/subsystems/intake/DirectIntake.cpp
+++ src/driftless/robot/subsystems/intake/DirectIntake.cpp
@@ -4,14 +4,78 @@ namespace driftless {
 namespace robot {
 namespace subsystems {
 namespace intake {
-void DirectIntake::init() {}
+void DirectIntake::taskLoop(void* params) {
+  DirectIntake* direct_intake{static_cast<DirectIntake*>(params)};
 
-void DirectIntake::run() {}
+  while (true) {
+    direct_intake->taskUpdate();
+  }
+}
 
-void DirectIntake::setVoltage(double voltage) { m_motors.setVoltage(voltage); }
+void DirectIntake::taskUpdate() {
+  if (m_mutex) {
+    m_mutex->take();
+  }
+
+  unjam();
+
+  if (m_mutex) {
+    m_mutex->give();
+  }
+  m_delayer->delay(TASK_DELAY);
+}
+
+void DirectIntake::unjam() {
+  if (m_motors.getEfficiency() < 0.2) {
+    if (!jammed) {
+      jammed = true;
+      m_motors.setVoltage(-12.0);
+      latest_jam_time = m_clock->getTime();
+    }
+  }
+  if (jammed && m_clock->getTime() > latest_jam_time + 500) {
+    jammed = false;
+    m_motors.setVoltage(target_voltage);
+  }
+}
+
+void DirectIntake::init() { m_motors.init(); }
+
+void DirectIntake::run() { m_task->start(taskLoop, this); }
+
+void DirectIntake::setVoltage(double voltage) {
+  if (m_mutex) {
+    m_mutex->take();
+  }
+
+  target_voltage = voltage;
+  if (!jammed) {
+    m_motors.setVoltage(target_voltage);
+  }
+
+  if (m_mutex) {
+    m_mutex->give();
+  }
+}
 
 void DirectIntake::setMotors(driftless::hal::MotorGroup& motors) {
   m_motors = motors;
+}
+
+void DirectIntake::setClock(const std::unique_ptr<rtos::IClock>& clock) {
+  m_clock = clock->clone();
+}
+
+void DirectIntake::setDelayer(const std::unique_ptr<rtos::IDelayer>& delayer) {
+  m_delayer = delayer->clone();
+}
+
+void DirectIntake::setMutex(std::unique_ptr<rtos::IMutex>& mutex) {
+  m_mutex = std::move(mutex);
+}
+
+void DirectIntake::setTask(std::unique_ptr<rtos::ITask>& task) {
+  m_task = std::move(task);
 }
 }  // namespace intake
 }  // namespace subsystems
