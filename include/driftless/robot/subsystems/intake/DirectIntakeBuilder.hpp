25560ffe5591b9df8bdc30e29cb5fbd3db01d790
backmat30 <matthewbackman@outlook.com>
2025-03-17 13:45:07 -0500

    Added rtos objects to the intake builder

--- include/driftless/robot/subsystems/intake/DirectIntakeBuilder.hpp
+++ include/driftless/robot/subsystems/intake/DirectIntakeBuilder.hpp
@@ -26,11 +26,39 @@ class DirectIntakeBuilder {
   // the motors used for the intake
   driftless::hal::MotorGroup m_motors{};
 
+  std::unique_ptr<rtos::IClock> m_clock{};
+
+  std::unique_ptr<rtos::IDelayer> m_delayer{};
+
+  std::unique_ptr<rtos::IMutex> m_mutex{};
+
+  std::unique_ptr<rtos::ITask> m_task{};
+
  public:
   /// @brief Adds motors to the builder
   /// @param motor __std::unique_ptr<driftless::io::IMotor>&__ The motor to add
   /// @return __DirectIntakeBuilder*__ The builder instance
   DirectIntakeBuilder* withMotor(std::unique_ptr<driftless::io::IMotor>& motor);
+
+  /// @brief Adds a clock to the builder
+  /// @param clock __const std::unique_ptr<rtos::IClock>&__ The clock to add
+  /// @return __DirectIntakeBuilder*__ The builder instance
+  DirectIntakeBuilder* withClock(const std::unique_ptr<rtos::IClock>& clock);
+  
+  /// @brief Adds a delayer to the builder
+  /// @param delayer __const std::unique_ptr<rtos::IDelayer>&__ The delayer to add
+  /// @return __DirectIntakeBuilder*__ The builder instance
+  DirectIntakeBuilder* withDelayer(const std::unique_ptr<rtos::IDelayer>& delayer);
+
+  /// @brief Adds a mutex to the builder
+  /// @param mutex __std::unique_ptr<rtos::IMutex>&__ The mutex to add
+  /// @return __DirectIntakeBuilder*__ The builder instance
+  DirectIntakeBuilder* withMutex(std::unique_ptr<rtos::IMutex>& mutex);
+
+  /// @brief Adds a task to the builder
+  /// @param task __std::unique_ptr<rtos::ITask>&__ The task to add
+  /// @return __DirectIntakeBuilder*__ The builder instance
+  DirectIntakeBuilder* withTask(std::unique_ptr<rtos::ITask>& task);
 
   /// @brief Builds a new DirectIntake object
   /// @return __std::unique_ptr<DirectIntake>__ The built DirectIntake object
--- src/driftless/robot/subsystems/intake/DirectIntakeBuilder.cpp
+++ src/driftless/robot/subsystems/intake/DirectIntakeBuilder.cpp
@@ -10,9 +10,37 @@ DirectIntakeBuilder* DirectIntakeBuilder::withMotor(
   return this;
 }
 
+DirectIntakeBuilder* DirectIntakeBuilder::withClock(
+    const std::unique_ptr<rtos::IClock>& clock) {
+  m_clock = clock->clone();
+  return this;
+}
+
+DirectIntakeBuilder* DirectIntakeBuilder::withDelayer(
+    const std::unique_ptr<rtos::IDelayer>& delayer) {
+  m_delayer = delayer->clone();
+  return this;
+}
+
+DirectIntakeBuilder* DirectIntakeBuilder::withMutex(
+    std::unique_ptr<rtos::IMutex>& mutex) {
+  m_mutex = std::move(mutex);
+  return this;
+}
+
+DirectIntakeBuilder* DirectIntakeBuilder::withTask(
+    std::unique_ptr<rtos::ITask>& task) {
+  m_task = std::move(task);
+  return this;
+}
+
 std::unique_ptr<DirectIntake> DirectIntakeBuilder::build() {
   std::unique_ptr<DirectIntake> intake{std::make_unique<DirectIntake>()};
   intake->setMotors(m_motors);
+  intake->setClock(m_clock);
+  intake->setDelayer(m_delayer);
+  intake->setMutex(m_mutex);
+  intake->setTask(m_task);
 
   return intake;
 }
