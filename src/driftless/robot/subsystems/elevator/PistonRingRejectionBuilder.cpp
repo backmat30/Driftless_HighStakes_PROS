eee75f974f63896a9447d77e8da1d86b0c474b66
backmat30 <matthewbackman@outlook.com>
2025-04-30 15:40:54 -0500

    Added the new piston to the builder for piston ring rejectors

--- src/driftless/robot/subsystems/elevator/PistonRingRejectionBuilder.cpp
+++ src/driftless/robot/subsystems/elevator/PistonRingRejectionBuilder.cpp
@@ -4,9 +4,15 @@ namespace driftless {
 namespace robot {
 namespace subsystems {
 namespace elevator {
-PistonRingRejectionBuilder* PistonRingRejectionBuilder::withPiston(
+PistonRingRejectionBuilder* PistonRingRejectionBuilder::withLeftPiston(
     std::unique_ptr<driftless::io::IPiston>& piston) {
-  m_pistons.addPiston(piston);
+  m_left_pistons.addPiston(piston);
+  return this;
+}
+
+PistonRingRejectionBuilder* PistonRingRejectionBuilder::withRightPiston(
+    std::unique_ptr<driftless::io::IPiston>& piston) {
+  m_right_pistons.addPiston(piston);
   return this;
 }
 
@@ -14,7 +20,8 @@ std::unique_ptr<IRingRejection> PistonRingRejectionBuilder::build() {
   std::unique_ptr<PistonRingRejection> piston_ring_rejection{
       std::make_unique<PistonRingRejection>()};
 
-  piston_ring_rejection->setPistons(m_pistons);
+  piston_ring_rejection->setLeftPistons(m_left_pistons);
+  piston_ring_rejection->setRightPistons(m_right_pistons);
 
   return piston_ring_rejection;
 }
