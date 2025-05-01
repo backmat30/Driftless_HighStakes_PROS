aa076505aa20bad73989bf5ec0a732c22fcc8034
backmat30 <matthewbackman@outlook.com>
2025-05-01 11:57:09 -0500

    Updated odom position

--- include/driftless/config/OrangeConfig.hpp
+++ include/driftless/config/OrangeConfig.hpp
@@ -406,7 +406,7 @@ class OrangeConfig : public IConfig {
 
   static constexpr double ODOMETRY_SENSOR_LOCAL_Y_OFFSET{-1.125};
 
-  static constexpr double ODOMETRY_SENSOR_LOCAL_THETA_OFFSET{0.0};
+  static constexpr double ODOMETRY_SENSOR_LOCAL_THETA_OFFSET{M_PI};
 
   static constexpr int ODOMETRY_BAUD_RATE{74880};
 
